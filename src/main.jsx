dfad3bce3dfb69219a18ad4011006225e8a1817c
shongCoder <soyeong163@gmail.com>
2025-02-26 04:51:34 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import mainRouter from "./routers/mainRouter.jsx";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <div className="font-sans">
+    <RouterProvider router={mainRouter}></RouterProvider>
+  </div>
+);
